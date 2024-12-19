ab4c3fda37aa43b110608c0f77ec866b1d832dc0
zera1004 <sizera1004@gmail.com>
2024-12-19 20:14:05 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,8 @@
+function solution(numbers, n) {
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        result += numbers[i];
+        if (result > n) break;
+    }
+    return result;
+}
