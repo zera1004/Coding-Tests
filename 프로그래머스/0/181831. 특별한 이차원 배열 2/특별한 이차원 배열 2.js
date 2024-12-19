6482ea66514549276e57856a9169bb63c8fa6105
zera1004 <sizera1004@gmail.com>
2024-12-19 20:32:13 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 2.24 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = arr.length-1; j >= 0; j--) {
+            if (arr[i][j] != arr[j][i]) {
+                console.log(i,j);
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
