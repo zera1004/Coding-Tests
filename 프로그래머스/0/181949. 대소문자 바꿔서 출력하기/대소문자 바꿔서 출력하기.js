2ec1668af7d4ee8fd24722704a36f619ade8c495
zera1004 <sizera1004@gmail.com>
2024-12-19 20:36:29 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 69.25 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    console.log(str.split('').map(i => {
+        if(i >= 'a' && i <= 'z') return i.toUpperCase()
+        if(i >= 'A' && i <= 'Z') return i.toLowerCase()
+    }).join(''))
+});
