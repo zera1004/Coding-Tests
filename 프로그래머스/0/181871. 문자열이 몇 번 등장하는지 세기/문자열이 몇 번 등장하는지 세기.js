49c49698a256927b31f9e2a24c5233648dc7bb63
zera1004 <sizera1004@gmail.com>
2024-12-21 02:57:26 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,5 +1,4 @@
 function solution(myString, pat) {
-    let temp = [];
     let result = 0;
     for (let i = 0; i <= myString.length - pat.length; i++) {
         if (myString.slice(i, i + pat.length) === pat) result++;
