6dc11a21a7dfd64cfb380aa207e11188f7510abb
zera1004 <sizera1004@gmail.com>
2024-12-28 23:15:12 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,6 @@
+function solution(date1, date2) {
+    if(date1[0] < date2[0]) return 1;
+    if(date1[0] === date2[0] && date1[1] < date2[1]) return 1;
+    if(date1[0] === date2[0] && date1[1] === date2[1] && date1[2] < date2[2]) return 1;
+    return 0;
+}
