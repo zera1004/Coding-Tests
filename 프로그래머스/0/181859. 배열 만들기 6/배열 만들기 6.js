c52bc317e9dfea207e47186af7c266f1692ae490
zera1004 <sizera1004@gmail.com>
2025-01-05 06:04:49 +0900

    [level 0] Title: 배열 만들기 6, Time: 218.98 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    while(i < arr.length) {
+        if(stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+        }
+        else if(stk.length !== 0 && stk[stk.length - 1] === arr[i]) {
+            stk = stk.slice(0, stk.length - 1);
+            i++;
+        }
+        else if(stk.length !== 0 && stk[stk.length - 1] !== arr[i]) {
+            stk.push(arr[i]);
+            i++;
+        }
+    }
+    return stk.length !== 0? stk: [-1]
+}
