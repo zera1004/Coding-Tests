4516e0a60b4712ed04a9913fd9f92521c6bbe8aa
zera1004 <sizera1004@gmail.com>
2025-02-19 00:32:17 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,2 @@
+def solution(date1, date2):
+    return 1 if date1[0] < date2[0] else (1 if date1[0] == date2[0] and date1[1] < date2[1] else (1 if date1[0] == date2[0] and date1[1] == date2[1] and date1[2] < date2[2] else 0))
